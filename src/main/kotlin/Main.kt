cb35adc30e1f6e966e98a7573e50930019470f56
Danieer Galvez <Moldycord@gmail.com>
2024-03-04 22:05:01 -0500

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,6 @@ package org.danieer.com
 
 import org.danieer.com.controllers.MenuController
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val menuController = MenuController()
     menuController.showMenu()
